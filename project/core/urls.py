8a3c90cba1619c457f6b93229b8ce0e411d1ed2e
Marios Koliouskas <marios.koliouskas@gmail.com>
2026-06-11 14:05:10 +0300

    Details pages for all objects

--- project/core/urls.py
+++ project/core/urls.py
@@ -5,6 +5,11 @@ from . import views
 urlpatterns = [
     #Define path, execute view function and give the name to this route for easy use(elsewhere)
     path("authors/", views.author_list, name="author_list"),
+    path("authors/<int:author_id>/", views.author_detail, name="author_detail"),
+
     path("books/", views.book_list, name="book_list"),
+    path("books/<int:book_id>/", views.book_detail, name="book_detail"),
+    
     path("categories/", views.category_list, name="category_list"),
+    path("categories/<int:category_id>/", views.category_detail, name="category_detail"),
 ]
--- project/core/views.py
+++ project/core/views.py
@@ -1,5 +1,5 @@
 #Recieves an HTML template, it fills it with data and returns a page to the browser
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Author, Book, Category
 
@@ -12,11 +12,25 @@ def author_list(request):
     authors = Author.objects.all()
     #Open the HTML file author_list.html and send the variable authors
     return render(request, "core/author_list.html", {"authors": authors})
+    
+def author_detail(request, author_id):
+    #Take author with the id given or 404 page
+    author = get_object_or_404(Author, id = author_id)
+    return render(request, "core/author_detail.html", {"author": author})
 
 def book_list(request):
     books = Book.objects.all()
     return render(request, "core/book_list.html", {"books": books})
 
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, id = book_id)
+    return render(request, "core/book_detail.html", {"book": book})
+
 def category_list(request):
     categories = Category.objects.all()
     return render(request, "core/category_list.html", {"categories": categories})
+
+def category_detail(request, category_id):
+    category = get_object_or_404(Category, id = category_id)
+    return render(request, "core/category_detail.html", {"category": category})
+
